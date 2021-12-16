9ff543456f7472ba8286b34759046c1b57880ee2
Markus Pauer <markus.pauer@gedoplan.de>
2021-12-16 19:46:57 +0100

    Cleanup Rest-Application

--- src/main/java/de/gedoplan/showcase/jnosql/RestApplication.java
+++ src/main/java/de/gedoplan/showcase/jnosql/RestApplication.java
@@ -1,18 +1,13 @@
 package de.gedoplan.showcase.jnosql;
 
-import java.util.HashSet;
-import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+/**
+ *
+ * @author Markus Pauer <markus.pauer@gedoplan.de>
+ */
 @ApplicationPath("api")
 public class RestApplication extends Application {
 
-//    @Override
-//    public Set<Class<?>> getClasses() {
-//        Set<Class<?>> classes = new HashSet<>();
-//        classes.add(de.gedoplan.showcase.jnosql.resources.PersonEndpoint.class);
-//        return classes;
-//    }
-    
 }
